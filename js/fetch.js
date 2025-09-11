0f6aef02a6bb021ee4b9c11f3731d4737f4e395a
anntrofim-debug <ann.trofim@gmail.com>
2025-09-11 11:22:21 +0200

    ny

--- js/fetch.js
+++ js/fetch.js
@@ -1,21 +1,28 @@
-export async function fetchRecommended() {
+export async function fetchProducts() {
   try {
-    const response = await fetch("../data/recommended.json");
+    const response = await fetch("../data/productsGroups.json");
     const data = await response.json();
     return data;
-    
   } catch (error) {
     console.error("Error fetching or parsing data:", error);
   }
 }
 
+export async function fetchRecommended() {
+  try {
+    const response = await fetch("../data/recommended.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
 
 export async function fetchGallery() {
   try {
     const response = await fetch("../data/gallery.json");
     const data = await response.json();
     return data;
-
   } catch (error) {
     console.error("Error fetching or parsing data:", error);
   }
